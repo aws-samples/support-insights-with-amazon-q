6eed0a04bb0c3fdbbedd13c17f34074aed276f9d
Jonathan Delfour <3976137+Jonathan34@users.noreply.github.com>
2024-08-15 16:00:33 -0500

    Update the template to 3.11 and update teh support collector lambda

--- src/support_collector/individual-account-deployments/deploy_collector_lambda_member.sh
+++ src/support_collector/individual-account-deployments/deploy_collector_lambda_member.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "This script will deploy the solution to a single account.\n"
+printf "This script will deploy the solution to a single account.\n"
 
-echo "Enter the name of the S3 bucket to store your support data:"
+printf  "Enter the name of the S3 bucket to store your support data:\n"
 read SUPPORT_BUCKET_NAME
 
-echo "Invoking deploy_lambda_function.py..."
-python3 "$PWD/individual-account-deployments/deploy_lambda_function.py" --support-data-bucket-name $SUPPORT_BUCKET_NAME
+printf "Invoking deploy_lambda_function.py...\n"
+python3 "$PWD/deploy_lambda_function.py" --support-data-bucket-name $SUPPORT_BUCKET_NAME
